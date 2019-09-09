e9eb854a5665778c2a2a054b536420248c23a25d
Kevin Long <kevin.uu.long@gmail.com>
2019-09-08 23:47:38 -0400

    Updated UI
    Still essentially functionless

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 window.onload = function(){
     document.getElementById("newsession").addEventListener('click', function(){
-        document.getElementById("toptextmain").innerHTML = "select a </br>counselor";
+        /*document.getElementById("toptextmain").innerHTML = "select a </br>counselor";
         document.getElementById("newsession").style.display = "none";
         document.getElementById("resumesession").style.display = "none";
         document.getElementById("newsessionnext").style.display = "block";
-        document.getElementById("counselors").style.display = "grid";
+        document.getElementById("counselors").style.display = "grid";*/
+        document.getElementById("newsessionscreen").style.display = "block";
+        document.getElementById("mainmenu").style.display = "none";
         var fitties = fitty('.counselorname');
         fitties[0].fit();
     });
+
+    document.getElementById("login").addEventListener('click', function(){
+        /*document.getElementById("toptextmain").innerHTML = "select a </br>counselor";
+        document.getElementById("login").style.display = "none";
+        document.getElementById("pin").style.display = "none";
+        document.getElementById("newsessionnext").style.display = "block";
+        document.getElementById("counselors").style.display = "grid";*/
+        document.getElementById("mainmenu").style.display = "block";
+        document.getElementById("pinscreen").style.display = "none";
+        console.log("test");
+        var fitties = fitty('.conversationtitletext', {
+            maxSize: 60
+        });
+        fitties[0].fit();
+    });
+
 };
